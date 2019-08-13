452bc5a04edc0582a7e7fcecf5aa23459f7f8a1b
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2019-08-13 14:05:28 +0200

    Add Blazor template for the species builder.

---
+++ DIGOS.Ambassador.Transformations.Web/DIGOS.Ambassador.Transformations.Web.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace DIGOS.Ambassador.Transformations.Web.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
